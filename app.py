564d5ba07e86cc2029928a18bc32d96684dd3e81
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-05-12 21:23:04 +0700

    coba nambahin navbar & footer

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
         
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 # MAIN
 if __name__ == "__main__":
     app.run(debug=True)

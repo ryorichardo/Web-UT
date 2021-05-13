b7abf9e69e1aea14c1a00eb294f3448eea7c5bcc
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-05-13 14:47:12 +0700

    js bootstrap, active navbar, coba2 accordion

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
-
-        
-    return render_template("home.html")
+    return render_template("home.html", home=True, about=False)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', home=False, about=True)
 
 # MAIN
 if __name__ == "__main__":

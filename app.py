1e82c674e5ad3d147c843f49dbafeee87397ab81
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-05-16 12:31:23 +0700

    centering navbar, fixing color pallete and footer

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
-    return render_template("home.html", home=True, about=False)
+    return render_template("home.html", home=True, about=False, events=False)
+
+@app.route('/events')
+def events():
+    return render_template('events.html', home=False, about=False, events=True)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', home=False, about=True)
+    return render_template('about.html', home=False, about=True, events=False)
+
 
 # MAIN
 if __name__ == "__main__":

f0049f3b9c8837ea9ad22920cecbfccc961a455b
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-05-16 14:21:38 +0700

    ubah urutan navbar

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("home.html", home=True, about=False, events=False)
 
-@app.route('/events')
-def events():
-    return render_template('events.html', home=False, about=False, events=True)
-
 @app.route('/about')
 def about():
     return render_template('about.html', home=False, about=True, events=False)
 
+@app.route('/events')
+def events():
+    return render_template('events.html', home=False, about=False, events=True)
 
 # MAIN
 if __name__ == "__main__":
